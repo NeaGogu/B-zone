62fc8afe74f04e669aaa7c9bc31adea426b22d81
Kristopher Schlett <82911374+grmjw@users.noreply.github.com>
2023-02-27 14:44:49 +0000

    Basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom' 
+import Login from './pages/login/index.js'
+import Home from './pages/home/index.js'
 import './App.css';
 
 function App() {
   return (
-    <p>page</p>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
