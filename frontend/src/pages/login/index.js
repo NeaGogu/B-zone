be3fcfa485acf6f94b50becde5bf5f731d1dc514
grmjw <kris.schlett13@gmail.com>
2023-02-28 20:40:09 +0100

    added ui + improved routing

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,32 +1,18 @@
-import { CopyOutlined } from '@ant-design/icons';
-import {
-    Button,
-    Input,
-} from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Card, ConfigProvider, theme, Alert } from 'antd';
+import { useNavigate } from "react-router-dom";
+import './index.css'
 
-function signIn(user, passw) {
-    console.log("Sign in button clicked")
-    fetch("https://sep202302.bumbal.eu/api/v2/authenticate/sign-in", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "email": user,
-            "password": passw,
-            "return_jwt": true
-        })
-    })
-        .then((response) => console.log(response.json()))
-        .then((data) => console.log(data));
-    return undefined;
-}
+// used for dark mdoe
+const { darkAlgorithm } = theme;
 
-export default function Login(){
+export default function Login() {
+
+    const navigate = useNavigate();
     const username = "sep@bumbal.eu"
     const password = "cW$#Qbph0524";
     const token = "";
+    var verified;
 
     function signIn(user, passw) {
         fetch("https://sep202302.bumbal.eu/api/v2/authenticate/sign-in", {
@@ -41,19 +27,78 @@ export default function Login(){
                 "return_jwt": true
             })
         })
-            .then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((response) => {
+                
+                if (!response.ok){
+                    verified = false;
+                }
+                else {
+                    verified = true;
+                }
+                return response.json();
+
+
+                
+              })
+            .then((data) => {
+                if (!verified) {
+                    alert('Invalid Credentials')
+                }
+                else{
+                    localStorage.setItem('token', data.token)
+                    navigate('/home')
+                }
+                
+            })
         return undefined;
     }
 
-    return (<Input.Group compact>
-        <Input
-            style={{
-                width: 'calc(100% - 200px)',
-            }}
-            defaultValue="https://ant.design"
-        />
-        <Button type="primary" onClick={() => signIn(username, password)}>Submit</Button>
-    </Input.Group>);
+    const onFinish = (values) => {
+        var email = Object.values(values)[0];
+        var pass = Object.values(values)[1];
+        signIn(email, pass)
+    }
+
+    return (
+        // parent component, essentially acts as a background 
+        <div className='page'>
+            <ConfigProvider
+                // theme which we should use
+                theme={{
+                    token: {
+                        colorPrimary: "#ffd369",
+                        colorBgBase: "#222831",
+                        colorPrimaryBg: "#393E46",
+                        colorTextBase: "#eeeeee"
+                    },
+                    // renders antd components with dark mode
+                    algorithm: darkAlgorithm
+                }}
+            >
+                {/* Form Implementation */}
+                <Card className='form-card' >
+                    <Form name='login-form' className='login-form' onFinish={onFinish}>
+                        <Form.Item name='Email' rules={[{ required: true, message: 'Email is required!' }]}>
+                            <Input placeholder='Email' prefix={<UserOutlined />} />
+                        </Form.Item>
+
+                        <Form.Item name='Password' rules={[{ required: true, message: 'Password is required!' }]}>
+                            <Input.Password placeholder='Password' prefix={<LockOutlined />} />
+                        </Form.Item>
+
+                        <Form.Item >
+                            <Button type='primary' className='login-button' htmlType='submit'>
+                                Log in
+                            </Button>
+                            <a href='https://support.bumbal.eu/en/register' style={{ marginLeft: 35 }}> or Register!</a>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </ConfigProvider>
+
+            {/* Bee Image (maybe use a background pre made instead of an svg as component) */}
+            <img src='https://upload.wikimedia.org/wikipedia/commons/c/c3/Bee_-_The_Noun_Project.svg' alt='Bee SVG' className='bee' />
+        </div>
+    );
 
 }
