34f5f39d845ee6bc9bbdf9966b212ef9f5b431a9
andreacbeiu <a.c.beiu@student.tue.nl>
2023-02-28 14:37:27 +0100

    Created Signin function for login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,5 +1,59 @@
+import { CopyOutlined } from '@ant-design/icons';
+import {
+    Button,
+    Input,
+} from 'antd';
+
+function signIn(user, passw) {
+    console.log("Sign in button clicked")
+    fetch("https://sep202302.bumbal.eu/api/v2/authenticate/sign-in", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "email": user,
+            "password": passw,
+            "return_jwt": true
+        })
+    })
+        .then((response) => console.log(response.json()))
+        .then((data) => console.log(data));
+    return undefined;
+}
+
 export default function Login(){
-    return (
-        <p>hello</p>
-    );
+    const username = "sep@bumbal.eu"
+    const password = "cW$#Qbph0524";
+    const token = "";
+
+    function signIn(user, passw) {
+        fetch("https://sep202302.bumbal.eu/api/v2/authenticate/sign-in", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": user,
+                "password": passw,
+                "return_jwt": true
+            })
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+        return undefined;
+    }
+
+    return (<Input.Group compact>
+        <Input
+            style={{
+                width: 'calc(100% - 200px)',
+            }}
+            defaultValue="https://ant.design"
+        />
+        <Button type="primary" onClick={() => signIn(username, password)}>Submit</Button>
+    </Input.Group>);
+
 }
