bf8c019671cee99adaebb2149683c92479bfd680
Victor Asanache <victor.asanache@gmail.com>
2023-03-02 19:28:34 +0100

    API endpoint for zipcodes: Use the following body for a request {     "pc4":["5611", 9947", "3297"] } Returns a list containing the coordinates for each zipcode, or a list of strings containing invalid zipcodes (the last element in the list is an error message). Code cleaning still need to be done.

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -30,17 +30,61 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	_ = &http.Server{
+	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
 
-	infoLog.Printf("Starting server on %s", addr)
-
-	parseZipFile("./short.json")
-
-	//err := srv.ListenAndServe()
-	//errorLog.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
+
+//package main
+//
+//import (
+//	"flag"
+//	"log"
+//	"net/http"
+//	"os"
+//)
+//
+//type application struct {
+//	errorLog *log.Logger
+//	infoLog  *log.Logger
+//}
+//
+//func main() {
+//	addr := flag.String("addr", ":4000", "HTTP network address")
+//
+//	flag.Parse()
+//
+//	// Use log.New() to create a logger for writing information messages. This takes
+//	// three parameters: the destination to write the logs to (os.Stdout), a string
+//	// prefix for message (INFO followed by a tab), and flags to indicate what
+//	// additional information to include (local date and time). Note that the flags
+//	// are joined using the bitwise OR operator |.
+//	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+//	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+//
+//	app := &application{
+//		errorLog: errorLog,
+//		infoLog:  infoLog,
+//	}
+//
+//	_ = &http.Server{
+//		Addr:     *addr,
+//		ErrorLog: errorLog,
+//		Handler:  app.routes(),
+//	}
+//
+//	infoLog.Printf("Starting server on %s", addr)
+//
+//	parseZipFile("./data/short.json")
+//
+//	//err := srv.ListenAndServe()
+//	//errorLog.Fatal(err)
+//
+//}
