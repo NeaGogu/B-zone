e3d448d9e5b5ef1c238c5c12043a38a281207c95
Victor Asanache <victor.asanache@gmail.com>
2023-03-07 13:41:19 +0100

    Implemented the middleware

--- backend/cmd/web/routes.go
+++ backend/cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/middleware"
@@ -9,6 +10,37 @@ import (
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello brothers!"))
+}
+
+// JwtChecker
+//
+//	@Description: a middleware that checks the JWT sent with the request
+//				no JWT, request fails immediately
+//	@param next
+//	@return http.Handler
+func JwtChecker(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		url := "https://sep202302.bumbal.eu/api/v2/authenticate/check-token"
+		jwToken := r.Header.Get("Authorization") // -> "Bearer <TOKEN>"
+
+		// query BUMBAL /check-token -> GET req with TOKEN in body
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		req.Header.Add("Authorization", jwToken)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println(resp.StatusCode)
+			http.Error(w, resp.Status, resp.StatusCode)
+			return
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	}
+	return http.HandlerFunc(fn)
 }
 
 func (app *application) routes() http.Handler {
@@ -23,6 +55,7 @@ func (app *application) routes() http.Handler {
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+	router.Use(JwtChecker)
 
 	router.Route("/test_route", func(r chi.Router) {
 		r.Get("/", hello)
