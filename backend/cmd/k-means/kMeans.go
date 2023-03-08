b4bd4ffb90d4b177e1657bf9b0a8d4fb83f8b875
Lucas <47983556+Lucasmeteenc@users.noreply.github.com>
2023-03-08 10:31:20 +0100

    refactor

--- backend/cmd/k-means/kMeans.go
+++ backend/cmd/k-means/kMeans.go
@@ -11,10 +11,6 @@ import (
 	"github.com/mroth/weightedrand/v2"
 )
 
-type coordinate struct {
-	coordinate float64
-}
-
 type coordinates struct {
 	long float64
 	lat  float64
@@ -25,9 +21,20 @@ type centroid struct {
 	AssignedActivities openapi.ActivityModel
 }
 
-var ErrNrActivitiesTooSmall = errors.New("Number of activities smaller than number of clusters")
-var ErrNrCandidateCentersTooSmall = errors.New("Number of candidate smaller than or equal to 0")
-var ErrNoActivities = errors.New("Number of candidate smaller than or equal to 0")
+var ErrNrActivitiesTooSmall = errors.New("number of activities smaller than number of clusters")
+var ErrNrCandidateCentersTooSmall = errors.New("number of candidate smaller than or equal to 0")
+var ErrNoActivities = errors.New("number of candidate smaller than or equal to 0")
+
+// refactor since it is used multiple times
+var emptyCentroid = []centroid{
+	{
+		center: coordinates{
+			long: 0,
+			lat:  0,
+		},
+		AssignedActivities: openapi.ActivityModel{},
+	},
+}
 
 func kMeans(activities []openapi.ActivityModel, nrClusters int) ([][]openapi.ActivityModel, error) {
 
@@ -65,138 +72,113 @@ func initCentroids(activities []openapi.ActivityModel, nrClusters int, nrCandida
 
 	//throw error if nr of activities is smaller than or equal to 0
 	if len(activities) <= 0 {
-		return []centroid{
-			{
-				center: coordinates{
-					long: 0,
-					lat:  0,
-				},
-				AssignedActivities: openapi.ActivityModel{},
-			},
-		}, ErrNoActivities
+		return emptyCentroid, ErrNoActivities
 	}
 	//throw error if nrCluster is larger than length of activities or nrCandidateCenters smaller or equal to 0
 	if nrClusters > len(activities) {
-		return []centroid{
-			{
-				center: coordinates{
-					long: 0,
-					lat:  0,
-				},
-				AssignedActivities: activities[0],
-			},
-		}, ErrNrActivitiesTooSmall
+		return emptyCentroid, ErrNrActivitiesTooSmall
 	}
 	//throw error if nr of candidate centers is smaller or equal to 0
 	if nrCandidateCenters <= 0 {
-		return []centroid{
-			{
-				center: coordinates{
-					long: 0,
-					lat:  0,
-				},
-				AssignedActivities: activities[0],
-			},
-		}, ErrNrCandidateCentersTooSmall
+		return emptyCentroid, ErrNrCandidateCentersTooSmall
 	}
 
 	//initialize centroids
-	centroids := make([]centroid, 1, nrClusters)
+	centroids := make([]centroid, 0, nrClusters)
 	//intialize slice to hold candidate centers
-	candidateCenters := make([]centroid, 0, nrCandidateCenters)
+	var candidateCenters []centroid
+	var err error
 
 	//Sample l candidateCenters
+	candidateCenters = make([]centroid, nrCandidateCenters)
 	for i := 0; i < nrCandidateCenters; i++ {
 		randomInt := rand.Intn(len(activities))
-		long, err := strconv.ParseFloat(*activities[randomInt].AddressApplied.Longitude, 64)
-		lat, err := strconv.ParseFloat(*activities[randomInt].AddressApplied.Latitude, 64)
 
-		//return if converting throws error
+		//sample candidates using random number (only done for first centroid)
+		candidateCenters, err = sampleCandidateCentroids(activities, randomInt, candidateCenters)
+
 		if err != nil {
-			return []centroid{
-				centroid{
-					center: coordinates{
-						long: 0,
-						lat:  0,
-					},
-					AssignedActivities: activities[0],
-				},
-			}, err
+			return emptyCentroid, err
 		}
-
-		candidateCenters = append(candidateCenters, centroid{
-			center: coordinates{
-				long: long,
-				lat:  lat,
-			},
-			AssignedActivities: activities[i],
-		})
 	}
 
-	//variable to store the iteration with lowest cost
-	var lowestIterationCost int
-
 	//calculate iteration with lowest cost
-	for i := 0; i < nrCandidateCenters; i++ {
+	lowestCost := lowestcandidateCenterCost(activities, nrCandidateCenters, candidateCenters)
 
-		minCost := math.Inf(0)
+	//append lowestcost centroid
+	centroids = append(centroids, candidateCenters[lowestCost])
+
+	//Iteratively choose best cluster to add
+	for i := 0; i < nrClusters-1; i++ {
+		//clear old candidateCenters
+		candidateCenters = make([]centroid, nrCandidateCenters)
+		//calculate bias of points with higher bias towards points further away from previous centroid
+		choices := newChoices(activities, centroids[i])
+		//make random chooser with bias
+		chs, err := weightedrand.NewChooser(choices...)
+
+		if err != nil {
+			return emptyCentroid, err
+		}
+
+		//choose new candidate centers with bias towards points furtheraway
+		biasRandomInt := chs.Pick()
+		candidateCenters, err := sampleCandidateCentroids(activities, biasRandomInt, candidateCenters)
+
+		if err != nil {
+			return emptyCentroid, err
+		}
+		//calculate candidateCenter with lowest cost
+		lowestCost = lowestcandidateCenterCost(activities, nrCandidateCenters, candidateCenters)
+
+		//append lowestcost centroid
+		centroids = append(centroids, candidateCenters[lowestCost])
+	}
+	return centroids, nil
+}
+
+func sampleCandidateCentroids(activities []openapi.ActivityModel, randomInt int, candidateCenters []centroid) ([]centroid, error) {
+	long, err := strconv.ParseFloat(*activities[randomInt].AddressApplied.Longitude, 64)
+
+	//return if converting throws error
+	if err != nil {
+		return emptyCentroid, err
+	}
+
+	lat, err := strconv.ParseFloat(*activities[randomInt].AddressApplied.Latitude, 64)
+
+	//return if converting throws error
+	if err != nil {
+		return emptyCentroid, err
+	}
+
+	candidateCenters = append(candidateCenters, centroid{
+		center: coordinates{
+			long: long,
+			lat:  lat,
+		},
+		AssignedActivities: activities[randomInt],
+	})
+
+	return candidateCenters, nil
+}
+
+func lowestcandidateCenterCost(activities []openapi.ActivityModel, nrCandidateCenters int, candidateCenters []centroid) int {
+	var lowIterationCost = 0
+	minCost := math.Inf(0)
+	for i := 0; i < nrCandidateCenters; i++ {
 		var iterationCost float64
+
 		for j := 0; j < len(activities); j++ {
 			cost := distanceToCluster(candidateCenters[i], activities[i])
 			iterationCost += cost
 		}
 		if iterationCost < minCost {
 			minCost = iterationCost
-			lowestIterationCost = i
+			lowIterationCost = i
 		}
 	}
-
-	centroids[0] = candidateCenters[lowestIterationCost]
-
-	//Iteratively choose best cluster to add
-	for i := 0; i < nrClusters-1; i++ {
-		choices := newChoices(activities, centroids[i])
-		chs, _ := weightedrand.NewChooser(choices...)
-		for i := 0; i < nrCandidateCenters; i++ {
-			biasRandomInt := chs.Pick()
-			long, err := strconv.ParseFloat(*activities[biasRandomInt].AddressApplied.Longitude, 64)
-			lat, err := strconv.ParseFloat(*activities[biasRandomInt].AddressApplied.Latitude, 64)
-
-			//return if converting throws error
-			if err != nil {
-				return centroids, err
-			}
-
-			candidateCenters[i] = centroid{
-				center: coordinates{
-					long: long,
-					lat:  lat,
-				},
-				AssignedActivities: activities[i],
-			}
-		}
-
-		sampleCandidateCentroids(activities, 2, func(chs *weightedrand.Chooser[int, int]) int {
-			return chs.Pick()
-		})
-
-		//calculate iteration with lowest cost
-		for i := 0; i < nrCandidateCenters; i++ {
-
-			minCost := math.Inf(0)
-			var iterationCost float64
-			for j := 0; j < len(activities); j++ {
-				cost := distanceToCluster(candidateCenters[i], activities[i])
-				iterationCost += cost
-			}
-			if iterationCost < minCost {
-				minCost = iterationCost
-				lowestIterationCost = i
-			}
-		}
-		centroids = append(centroids, candidateCenters[lowestIterationCost])
-	}
-	return centroids, fmt.Errorf("weet nog niet welke error")
+	return lowIterationCost
 }
 
 func newChoices(activities []openapi.ActivityModel, centroid centroid) []weightedrand.Choice[int, int] {
@@ -207,25 +189,18 @@ func newChoices(activities []openapi.ActivityModel, centroid centroid) []weighte
 	//choices
 	choices := make([]weightedrand.Choice[int, int], 0, len(activities))
 	//calculate distance to centroid
-	for index, _ := range activities {
+	for index := range activities {
 		distanceToCentroid[index] = distanceToCluster(centroid, activities[index])
 		distanceSum += distanceToCentroid[index]
 	}
 
-	for index, _ := range activities {
+	for index := range activities {
 		distanceToCentroid[index] = distanceToCentroid[index] / distanceSum
-	}
-
-	for index, _ := range activities {
 		tempChoice := weightedrand.NewChoice(index, int(math.Round(distanceToCentroid[index])))
 		choices = append(choices, tempChoice)
 	}
 
 	return choices
-}
-
-func sampleCandidateCentroids[T any](activities []openapi.ActivityModel, nrCandidateCenters int, random T) {
-
 }
 
 // Clears all activities from given centroids
