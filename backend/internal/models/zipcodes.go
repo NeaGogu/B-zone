911d4329c338649db1597e7181db4100fe705e9b
vlad plaiasu <vlad.plaiasu.vp@gmail.com>
2023-03-10 13:23:50 +0100

    Issue #69 (nice) created method that queries the database for a range of zipcodes; updated zipCoordinates handler

--- backend/internal/models/zipcodes.go
+++ backend/internal/models/zipcodes.go
@@ -1,6 +1,15 @@
 package models
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const(
+	CoordCollection = "coordinates"
+)
 
 type ZipCodeDBModel struct {
 	DB *mongo.Database
@@ -8,8 +17,42 @@ type ZipCodeDBModel struct {
 
 type ZipCode struct {
 	Code string `json:"code" bson:"code"`
-	// PolygonCoordinates [][][]float64 `json:"zone_coordinates" bson:"zone_coordinates, omitempty"`
-	// MultiPolyCoordinates [][][][]float64 `json:"zone_coordinates" bson:"zone_coordinates, omitempty"`
 	Coordinates interface{} `json:"zone_coordinates" bson:"zone_coordinates"`
 	Type string `json:"type" bson:"type, omitempty"`
+}
+
+
+	// queryFilter := bson.D{{Key: "code", Value: reqZipCode}}
+	//
+	// // TODO add this collection as a constant
+	// coll := app.zipCodeDbModel.DB.Collection("coordinates")
+	//
+	// var zipCode models.ZipCode
+	// err := coll.FindOne(context.TODO(), queryFilter).Decode(&zipCode)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+func (z *ZipCodeDBModel) GetZipCodes(reqZipCodeFrom string, reqZipCodeTo string) ([]ZipCode, error) {
+	// get the coordinates collection
+	coll := z.DB.Collection(CoordCollection)
+
+
+	queryFilter := bson.M { "code" : bson.M {"$lte": reqZipCodeTo, "$gte": reqZipCodeFrom}}
+	cur, err:= coll.Find(context.TODO(), queryFilter)
+	if err != nil {
+		return nil, err
+	}
+
+	defer cur.Close(context.TODO())
+	// @see https://www.mongodb.com/docs/drivers/go/current/fundamentals/crud/read-operations/cursor/#std-label-golang-cursor
+	// TODO might want to initialize slice with a zise of cur.BatchSize
+	// TODO too many results might cause memory issues... need to investigate -> pagination?
+	var results []ZipCode
+	if err = cur.All(context.TODO(), &results); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }
