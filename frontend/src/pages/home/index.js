7248e9cffc2b276bbbeb2510de648b061fa00ab6
taniagoia <t.goia@student.tue.nl>
2023-03-17 12:34:43 +0100

    Sider Component created, need to fix issues

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -6,13 +6,14 @@ import "leaflet-defaulticon-compatibility";
 import { Layout, Menu, theme, Form, Input, Button, Dropdown, Space, ConfigProvider, Radio, InputNumber } from 'antd';
 
 // Icons
-import { UserOutlined, DeleteOutlined, SaveOutlined } from '@ant-design/icons';
+import { UserOutlined, SaveOutlined } from '@ant-design/icons';
 
 // Components
 import Map from './components/mapComponent';
 
 // CSS
 import './index.css';
+import SiderComponent from "./components/siderComponent";
 //import dumbzones from './tempData/allcases.json'
 
 // Components from Ant Design
@@ -83,52 +84,7 @@ function signOut() {
         })
 }
 
-//Input field function -> later on add calculations, for now checks if the two fields are filled and if so, then the button is activated
-const ZoneSubMenu = ({ onSubmit }) => {
-    const [averageFuelCost, setAverageFuelCost] = useState("");
-    const [averageFuelUsage, setAverageFuelUsage] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isValid = onSubmit(averageFuelCost, averageFuelUsage);
-        if (isValid) {
-            // Add calculations
-        }
-    };
-    // Input fields for the zone calculation (average fuel cost, average fuel usage of car).
-    return (
-        <Form onFinish={handleSubmit}>
-            <Form.Item rules={[{ required: true }]}>
-                Average fuel cost
-                <Input
-                    placeholder="input fuel cost"
-                    type="number"
-                    step="0.01"
-                    value={averageFuelCost}
-                    onChange={(e) => setAverageFuelCost(e.target.value)}
-                />
-            </Form.Item>
-            <Form.Item>
-                Average fuel usage of car
-                <Input
-                    placeholder="input fuel usage of car"
-                    type="number"
-                    step="0.01"
-                    value={averageFuelUsage}
-                    onChange={(e) => setAverageFuelUsage(e.target.value)}
-                />
-            </Form.Item>
-            <Button
-                style={{ width: "100%" }}
-                type="primary"
-                htmlType="submit"
-                disabled={!averageFuelCost || !averageFuelUsage}
-            >
-                Calculate
-            </Button>
-        </Form>
-    );
-};
 
 localStorage.getItem('token')
 
@@ -143,21 +99,6 @@ export default function Home() {
     // For view button to bring two maps back to one map.
     const [showMap, setShowMap] = useState(true);
 
-    // Toggles the splitting of the map into two maps.
-    const toggleComparison = () => {
-        if (showComparison) {
-            return; // If showComparison is already true, do nothing.
-        }
-
-        setShowComparison(true);
-        setShowMap(false); // Reset singular map state when switching to two maps.
-    }
-
-    // Toggles the bringing back of the two maps into one map.
-    const toggleMap = () => {
-        setShowMap(!showMap);
-        setShowComparison(false); // Reset comparison state when switching to singular map.
-    };
 
     // For radio.
     const [value, setValue] = useState(1);
@@ -187,13 +128,18 @@ export default function Home() {
             setSaveName(name);
         }
     }
-    
+
     // This makes sure the initial zone configuration is always shown.
     const [savedZones, setSavedZones] = useState([
         { key: 'saved-initial', name: 'Initial Zone' },
 
     ]);
     localStorage.setItem('saved-initial', 'Initial Zone');
+    const handleDeleteZone = (key) => {
+        localStorage.removeItem(key);
+        const newSavedZones = savedZones.filter((item) => item.key !== key);
+        setSavedZones(newSavedZones);
+    };
 
     /** 
     * When the user has entered a name for the zone configuration they want to save, it is shown in the saved zones list.
@@ -272,67 +218,40 @@ export default function Home() {
                             background: colorBgContainer,
                         }}
                     >
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{
-                                height: '100%',
-                                borderRight: 0,
-                            }}
-                        >
-                            <SubMenu key="sub3" title="Heat map" style={{}}>
-                                <Menu.Item key="5" style={{ padding: 0 }}>
-                                    <Radio.Group value={value} onChange={onChange} size='small'  >
-                                        <Radio.Button value={1}>
-                                            Time based
-                                        </Radio.Button>
-                                        <Radio.Button value={2}>
-                                            Location based
-                                        </Radio.Button>
-                                    </Radio.Group>
-                                </Menu.Item>
-                                <Menu.Item key="6" style={{ height: "80px", padding: 0 }}>
-                                    <div style={{ textAlign: "center" }}>Intensity</div>
-                                    <div style={{ paddingLeft: 50 }}>
-                                        <InputNumber min={1} max={1000} defaultValue={500} onChange={onChangeNumber} disabled={value === 1 ? true : false} />
-                                    </div>
-                                </Menu.Item>
-                            </SubMenu>
-                            <SubMenu key="sub4" title="Zones">
-                                <ZoneSubMenu />
-                            </SubMenu>
+                        //heatmap settings as Menu for testing purposes 
+                            {/*<Menu key="sub3" title="Heat map" style={{}}>*/}
+                            {/*    <Menu.Item key="5" style={{ padding: 0 }}>*/}
+                            {/*        <Radio.Group value={value} onChange={onChange} size='small'  >*/}
+                            {/*            <Radio.Button value={1}>*/}
+                            {/*                Time based*/}
+                            {/*            </Radio.Button>*/}
+                            {/*            <Radio.Button value={2}>*/}
+                            {/*                Location based*/}
+                            {/*            </Radio.Button>*/}
+                            {/*        </Radio.Group>*/}
+                            {/*    </Menu.Item>*/}
+                            {/*    <Menu.Item key="6" style={{ height: "80px", padding: 0 }}>*/}
+                            {/*        <div style={{ textAlign: "center" }}>Intensity</div>*/}
+                            {/*        <div style={{ paddingLeft: 50 }}>*/}
+                            {/*            <InputNumber min={1} max={1000} defaultValue={500} onChange={onChangeNumber} disabled={value === 1 ? true : false} />*/}
+                            {/*        </div>*/}
+                            {/*    </Menu.Item>*/}
+                            {/*</Menu>*/}
 
-                            <SubMenu key="sub2" title="Saved Zones">
-                                {savedZones.map((zone) => (
-                                    <Menu.Item key={zone.key} style={{ height: '80px', padding: 0 }}>
-                                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                            <span style={{ paddingLeft: '10px' }}>{zone.name}</span>
-                                            {zone.name !== 'Initial Zone' && (
-                                                <Button
-                                                    style={{ float: 'right' }}
-                                                    onClick={() => {
-                                                        localStorage.removeItem(zone.key);
-                                                        const newSavedZones = savedZones.filter((item) => item.key !== zone.key);
-                                                        setSavedZones(newSavedZones);
-                                                    }}
-                                                >
-                                                    <DeleteOutlined />
-                                                </Button>
-                                            )}
-                                        </div>
-                                        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                                            <Button style={{ flex: 1, marginRight: '3px' }} onClick={toggleMap}>
-                                                View
-                                            </Button>
-                                            <Button style={{ flex: 1, marginLeft: '3px' }} onClick={toggleComparison}>
-                                                Compare
-                                            </Button>
-                                        </div>
-                                    </Menu.Item>
-                                ))}
-                            </SubMenu>
-                        </Menu>
+                            //sider component
+                            <SiderComponent
+                                savedZones={savedZones}
+                                setSavedZones={setSavedZones}
+                                onDeleteZone={handleDeleteZone}
+                                showMap={showMap}
+                                setShowMap={setShowMap}
+                                showComparison={showComparison}
+                                setShowComparison={setShowComparison}
+                                value={value}
+                                onChange={onChange}
+                                intensity={intensity}
+                                onChangeNumber={onChangeNumber}
+                            />
                     </Sider>
 
                     <Layout style={{
@@ -347,7 +266,7 @@ export default function Home() {
                             {showComparison ? (
                                 <div style={{ display: "flex", justifyContent: "space-between", padding: "5px" }}>
                                     <Map /> 
-                                    <Map />
+                                    <Map intensity={intensity} value={value} onChange={onChange} onChangeNumber={onChangeNumber}/>
                                 </div>
                             ) : (
                                 <Map /> 
