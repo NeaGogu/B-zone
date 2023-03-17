9ebd151bb75c6f1de74bd143b32b1220e2fd27e8
vlad plaiasu <vlad.plaiasu.vp@gmail.com>
2023-03-17 12:21:37 +0100

    moved stuff around and removed go.mod from swag_gen

--- backend/go.mod
+++ backend/go.mod
@@ -4,18 +4,23 @@ go 1.19
 
 require (
 	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/cors v1.2.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	go.mongodb.org/mongo-driver v1.11.2
+	golang.org/x/oauth2 v0.6.0
 )
 
-require github.com/google/uuid v1.3.0 // indirect
+require github.com/google/uuid v1.3.0
 
 require (
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
 
 require (
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/chi/v5 v5.0.8
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
@@ -27,5 +32,5 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
--- backend/internal/models/bzone_models.go
+++ backend/internal/models/bzone_models.go
@@ -1,11 +1,8 @@
 package models
 
 import (
-	"context"
-	"errors"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -80,61 +77,3 @@ type ZoneRangeModel struct {
 	//array of coordinates?
 }
 
-// helper method to query the database for a zone by id
-// pretty much obsolete because GetZonesListById can behave in the same way
-// if passed only one id
-func (m *BzoneDBModel) GetZoneById(zoneId string) (*ZoneModel, error) {
-
-	// Get the zones collection
-	coll := m.DB.Collection(ZoneCollection)
-
-	// Create a filter with the zone id
-	queryFilter := bson.M{"zone_id": zoneId}
-
-	// Create a variable to store the decoded zone
-	var zone ZoneModel
-	err := coll.FindOne(context.TODO(), queryFilter).Decode(&zone)
-	if err != nil {
-		// if the query does not find any result, then FindOne will return a
-		// mongo.ErrNoDocuments error. We can check for this error and return
-		// our own ErrDocumentNotFound error instead so that it can be handled
-		// differently
-		if errors.Is(err, mongo.ErrNoDocuments) {
-			return nil, ErrDocumentNotFound
-		}
-
-		// otherwise, return the error
-		return nil, err
-	}
-
-	return &zone, nil
-
-}
-
-// helper method to query the database for a list of zones
-func (m *BzoneDBModel) GetZonesListById(zoneIds ...string) ([]*ZoneModel, error) {
-
-	// Get the zones collection
-	coll := m.DB.Collection(ZoneCollection)
-
-	queryFilter := bson.M{"zone_id": bson.M{"$in": zoneIds}}
-
-	cur, err := coll.Find(context.TODO(), queryFilter)
-	if err != nil {
-		return nil, err
-	}
-
-	// don't forget to close the cursor when you're done
-	defer cur.Close(context.TODO())
-
-	var result []*ZoneModel
-	if err = cur.All(context.TODO(), &result); err != nil {
-		return nil, err
-	}
-
-	if len(result) == 0 {
-		return nil, ErrDocumentNotFound
-	}
-
-	return result, nil
-}
