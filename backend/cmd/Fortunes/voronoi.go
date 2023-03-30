941dd383b1d96f7443fb91353d75e3fd1696541c
Lucas <47983556+Lucasmeteenc@users.noreply.github.com>
2023-03-30 09:40:28 +0200

    TestClusterToVertex

--- backend/cmd/Fortunes/voronoi.go
+++ backend/cmd/Fortunes/voronoi.go
@@ -2,96 +2,149 @@ package main
 
 import (
 	kMeans "bzone/backend/cmd/k-means"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math/rand"
+	"os"
 
 	voronoi "github.com/pzsz/voronoi"
 )
 
-func VoronoiDiagram(clusters kMeans.Clusters) *voronoi.Diagram {
-	//convert clusters to vertices (input used by voronoi)
-	vertices := clusterToVertexList(clusters)
-	//Generate the voronoi diagram
-	diagram := generateVoronoiDiagram(vertices)
-	return diagram
-}
+//	func VoronoiDiagram(clusters kMeans.Clusters) *voronoi.Diagram {
+//		//convert clusters to vertices (input used by voronoi)
+//		vertices := clusterToVertexList(clusters)
+//		//Generate the voronoi diagram
+//		diagram := generateVoronoiDiagram(vertices)
+//		return diagram
+//	}
+
+var coordinateMultiplier = 10.0
+var xl = 3.1
+var xr = 7.1
+var yt = 50.6
+var yb = 53.7
 
 func clusterToVertexList(clusters kMeans.Clusters) []voronoi.Vertex {
 	var sites []voronoi.Vertex
-
 	for _, cluster := range clusters {
-		sites = append(sites, voronoi.Vertex{X: cluster.Center.Latitude, Y: cluster.Center.Longitude})
+		//check if x coordinate is within the Netherlands
+		x := cluster.Center.Longitude
+		if x < xl {
+			fmt.Fprintf(os.Stderr, "x coordinate too small, adjusting to the border: %f", x)
+			x = xl
+		}
+		if x > xr {
+			fmt.Fprintf(os.Stderr, "x coordinate too large, adjusting to the border: %f", x)
+			x = xr
+		}
+
+		//check if y coordinate is within the Netherlands
+		y := cluster.Center.Latitude
+		if y < yt {
+			fmt.Fprintf(os.Stderr, "y coordinate too small, adjusting to the border: %f", y)
+			y = yt
+		}
+		if y > yb {
+			fmt.Fprintf(os.Stderr, "y coordinate too large, adjusting to the border: %f", y)
+			y = yb
+		}
+		sites = append(sites, voronoi.Vertex{X: x * coordinateMultiplier, Y: y * coordinateMultiplier})
 	}
+
 	return sites
 }
 
-func generateVoronoiDiagram(vertices []voronoi.Vertex) *voronoi.Diagram {
-	bbox := voronoi.NewBBox(3.23, 7.3, 6.5, 5.92)
-	voronoiDiagram := voronoi.ComputeDiagram(vertices, bbox, true)
-	return voronoiDiagram
+// func generateVoronoiDiagram(vertices []voronoi.Vertex) *voronoi.Diagram {
+// 	bbox := voronoi.NewBBox(3.23, 7.3, 6.5, 5.92)
+// 	voronoiDiagram := voronoi.ComputeDiagram(vertices, bbox, true)
+// 	return voronoiDiagram
+// }
+
+func voronoiDiagram(clusters kMeans.Clusters) {
+	// Set up the Voronoibox for the Netherlands
+	bbox := voronoi.NewBBox(xl*coordinateMultiplier, xr*coordinateMultiplier, yt*coordinateMultiplier, yb*coordinateMultiplier)
+	//sites := generateRandomPoints(50, bbox)
+
+	//convert clusters to vertices
+	sites := clusterToVertexList(clusters)
+
+	v := voronoi.ComputeDiagram(sites, bbox, true)
+
+	// Create the output image
+	img := image.NewRGBA(image.Rect(0, 0, int(bbox.Xl-bbox.Xr), int(bbox.Yt-bbox.Yb)))
+	drawSites(img, sites, bbox)
+	drawEdges(img, v.Edges, bbox)
+
+	// Save the image to a file
+	f, err := os.Create("voronoi.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	err = png.Encode(f, img)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-// func createVoronoiImage(width, height, numSites int) image.Image {
-// 	diagram := generateVoronoiDiagram(width, height, numSites)
+func generateRandomPoints(n int, bbox voronoi.BBox) []voronoi.Vertex {
+	var points []voronoi.Vertex
+	for i := 0; i < n; i++ {
+		x := rand.Float64()*(bbox.Xl-bbox.Xr) + bbox.Xr
+		fmt.Println(x)
+		y := rand.Float64()*(bbox.Yt-bbox.Yb) + bbox.Yb
+		points = append(points, voronoi.Vertex{x, y})
+	}
+	return points
+}
 
-// 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-// 	for _, edge := range diagram.Edges {
+func drawSites(img *image.RGBA, sites []voronoi.Vertex, bbox voronoi.BBox) {
+	for _, site := range sites {
+		x, y := int(site.X-bbox.Xr), int(site.Y-bbox.Yb)
+		img.Set(x, y, color.Black)
+	}
+}
 
-// 		x0, y0 := int(edge.Va.X), int(edge.Va.Y)
-// 		x1, y1 := int(edge.Vb.X), int(edge.Vb.Y)
+func drawEdges(img *image.RGBA, edges []*voronoi.Edge, bbox voronoi.BBox) {
+	for _, edge := range edges {
+		x1, y1 := int(edge.Va.X-bbox.Xr), int(edge.Va.Y-bbox.Yb)
+		x2, y2 := int(edge.Vb.X-bbox.Xr), int(edge.Vb.Y-bbox.Yb)
+		drawLine(img, x1, y1, x2, y2, color.Black)
+	}
+}
 
-// 		drawLine(img, x0, y0, x1, y1, color.Black)
-// 	}
+func drawLine(img *image.RGBA, x1, y1, x2, y2 int, c color.Color) {
+	dx := x2 - x1
+	dy := y2 - y1
+	if dx == 0 && dy == 0 {
+		img.Set(x1, y1, c)
+		return
+	}
+	if abs(dx) >= abs(dy) {
+		if x2 < x1 {
+			x1, y1, x2, y2 = x2, y2, x1, y1
+		}
+		for x := x1; x <= x2; x++ {
+			y := y1 + dy*(x-x1)/dx
+			img.Set(x, y, c)
+		}
+	} else {
+		if y2 < y1 {
+			x1, y1, x2, y2 = x2, y2, x1, y1
+		}
+		for y := y1; y <= y2; y++ {
+			x := x1 + dx*(y-y1)/dy
+			img.Set(x, y, c)
+		}
+	}
+}
 
-// 	return img
-// }
-
-// func drawLine(img *image.RGBA, x0, y0, x1, y1 int, c color.Color) {
-// 	dx := abs(x1 - x0)
-// 	dy := abs(y1 - y0)
-// 	sx := -1
-// 	if x0 < x1 {
-// 		sx = 1
-// 	}
-// 	sy := -1
-// 	if y0 < y1 {
-// 		sy = 1
-// 	}
-// 	err := dx - dy
-// 	for {
-// 		img.Set(x0, y0, c)
-// 		if x0 == x1 && y0 == y1 {
-// 			break
-// 		}
-// 		e2 := 2 * err
-// 		if e2 > -dy {
-// 			err -= dy
-// 			x0 += sx
-// 		}
-// 		if e2 < dx {
-// 			err += dx
-// 			y0 += sy
-// 		}
-// 	}
-// }
-
-// func abs(x int) int {
-// 	if x < 0 {
-// 		return -x
-// 	}
-// 	return x
-// }
-
-// func main() {
-
-// 	width := 800
-// 	height := 600
-// 	numSites := 100
-
-// 	img := createVoronoiImage(width, height, numSites)
-// 	file, err := os.Create("voronoi.png")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer file.Close()
-
-// 	png.Encode(file, img)
-// }
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
---
+++ backend/cmd/Fortunes/voronoi_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"testing"
+
+	kMeans "bzone/backend/cmd/k-means"
+
+	voronoi "github.com/pzsz/voronoi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClusterToVertexList(t *testing.T) {
+	// Set up test data
+	tests := []struct {
+		name     string
+		clusters kMeans.Clusters
+		want     []voronoi.Vertex
+	}{
+		{
+			name: "Test with two clusters within bounds",
+			clusters: kMeans.Clusters{
+				{
+					Center: kMeans.Coordinates{Latitude: 52.370216, Longitude: 4.895168},
+				},
+				{
+					Center: kMeans.Coordinates{Latitude: 51.9194, Longitude: 4.4818},
+				},
+			},
+			want: []voronoi.Vertex{
+				{X: 48.9516800, Y: 523.702160},
+				{X: 44.8180000, Y: 519.194000},
+			},
+		},
+		{
+			name: "Test with one cluster outside bounds",
+			clusters: kMeans.Clusters{
+				{
+					Center: kMeans.Coordinates{Latitude: 53.4808, Longitude: -2.2426},
+				},
+			},
+			want: []voronoi.Vertex{
+				{X: 31, Y: 534.808},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := clusterToVertexList(tt.clusters)
+			// GIS/handheld gps precision
+			epsilon := 0.0001
+			// Check the result
+			if len(tt.want) != len(got) {
+				t.Fatalf("unexpected result length: got %d, want %d", len(got), len(tt.want))
+			}
+
+			for i, v := range got {
+				// if v.X < xl*coordinateMultiplier || v.X > xr*coordinateMultiplier || v.Y < yb*coordinateMultiplier || v.Y > yt*coordinateMultiplier {
+				// 	t.Errorf("result vertex %d is outside the Netherlands: %v", i, v)
+				// }
+
+				assert.InDelta(t, tt.want[i].X, v.X, epsilon, "X coordinates are not equal")
+				assert.InDelta(t, tt.want[i].Y, v.Y, epsilon, "Y coordinates are not equal")
+			}
+		})
+	}
+}
