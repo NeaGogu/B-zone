1b298bc21ecb5a2a0349df8c40c4f5affd3c4b62
fleurhamoen <fleurhamoen@outlook.com>
2023-04-04 15:04:56 +0200

    Fixed warnings and changed comments

--- frontend/src/pages/home/components/textComponent.js
+++ frontend/src/pages/home/components/textComponent.js
@@ -1,5 +1,4 @@
-//import 'leaflet.heat'
-import {Card, Dropdown, Spin, Tooltip} from 'antd'
+import {Card, Spin, Tooltip} from 'antd'
 import { useEffect, useState } from "react";
 import querryDatabase from "../functions/querryDatabase";
 import getAllActivities from "../functions/getAllActivities";
@@ -14,9 +13,6 @@ const { Panel } = Collapse;
 async function totalActivityDurations(settime) {
     const activities = await getAllActivities();
 
-
-
-    //console.log(activities)
     var data2 = []
     var time = 0;
     for (let i = 0; i < activities.length; i++) {
@@ -33,8 +29,8 @@ async function totalActivityDurations(settime) {
 }
 
 async function getDrivingTime(drivingData) {
-    //input: a fetch response from OSRM with multiple legs per driving route
-    //output: the total sum of the duration over all the legs of the route
+    // Input: a fetch response from OSRM with multiple legs per driving route
+    // Output: the total sum of the duration over all the legs of the route
     let drivingLegs = drivingData.routes[0].legs
     let sum = 0
     for (let i = 0; i < drivingLegs.length; i++) {
@@ -44,10 +40,11 @@ async function getDrivingTime(drivingData) {
 }
 
 async function getDrivingDistance(drivingData) {
-    //input: a fetch response from OSRM with multiple legs per driving route
-    //output: the total sum of the distance over all the legs of the route
+    // Input: a fetch response from OSRM with multiple legs per driving route
+    // Output: the total sum of the distance over all the legs of the route
     let drivingLegs = drivingData.routes[0].legs
     let sum = 0
+    
     for (let i = 0; i < drivingLegs.length; i++) {
         sum = sum + drivingLegs[i].distance
     }
@@ -55,8 +52,7 @@ async function getDrivingDistance(drivingData) {
 }
 
 function TextComponent(props) {
-    const { zoneId, zoneName, calculatedZone, setAverageFuelUsage,
-        averageFuelCost,averageFuelUsage, setAverageFuelCost  } = props;
+    const { zoneId, zoneName, calculatedZone, averageFuelCost,averageFuelUsage  } = props;
 
     const [name, setName] = useState('')
     const [loaded, setLoaded] = useState(false)
@@ -65,151 +61,137 @@ function TextComponent(props) {
     const [fuelCost, setFuelCost] = useState(0)
     const [drivingTimeActiv, setDrivingTimeActiv] = useState([])
     const [drivingDistanceActiv, setDrivingDistanceActiv] = useState([])
-    let averageCost = 1.8
     let averageFuelConsumption = averageFuelUsage
-    let averageFuel = 0.047 //litres of fuel consumption per km
-
-
 
     useEffect(() => {
         const initial = async () => {
             setLoaded(false)
-            let averageFuelCost = 1.8 //cost in euros per litre of fuel
-            let averageFuelConsumption = 0.047 //litres of fuel consumption per km
-            totalActivityDurations(setTime) //set the total activity duration to be the time spend on activities
-            let plot = []; //variable to hold the object of zones/zone configuration upon which to calculate driving time
-            if (zoneId.startsWith('calculate')) { //the plot is a freshly calculated optimized plot
-                //plot = calculatedZone.plot_zones[1].zone_ranges //change this object model so that it can be used in the calculations below
+            let averageFuelCost = 1.8 // Cost in euros per litre of fuel
+            let averageFuelConsumption = 0.047 // Litres of fuel consumption per km
+            totalActivityDurations(setTime) // Set the total activity duration to be the time spend on activities
+            let plot = []; // Variable to hold the object of zones/zone configuration upon which to calculate driving time
+           
+            if (zoneId.startsWith('calculate')) { // The plot is a freshly calculated optimized plot
                  for (let i = 0; i < calculatedZone.plot_zones.length; i++) {
                      plot[i] = calculatedZone.plot_zones[i].zone_ranges
                      for(let j = 0; j < plot[i].length; j ++) {
                          plot[i][j] = {zipFrom: plot[i][j].zipcode_from, zipTo: plot[i][j].zipcode_to}
                      }
-
                  }
                 setName('Calculated Zone')
             }
+
             else {
-                if(zoneId.startsWith('initial')) { //the plot is the initial plot from Bumbal
+                if (zoneId.startsWith('initial')) { //the plot is the initial plot from Bumbal
                     setName(zoneName)
-                    for(let i = 0; i < calculatedZone.length; i++) {
-                        for(let j = 0; j < calculatedZone[i].length; j++) {
+                    for (let i = 0; i < calculatedZone.length; i++) {
+                        for (let j = 0; j < calculatedZone[i].length; j++) {
                             calculatedZone[i][j].zipFrom = parseInt(calculatedZone[i][j].zipFrom);
                             calculatedZone[i][j].zipTo = parseInt(calculatedZone[i][j].zipTo);
                         }
                     }
-                    plot = calculatedZone //retrieve the current plot and work with it
-                } else { //the plot is a saved plot from our backend
+                    plot = calculatedZone // Retrieve the current plot and work with it
+                } else { // The plot is a saved plot from the backend
                     plot = await querryDatabase(zoneId)
                     setName(zoneName)
                 }
-
             }
+
             console.log("The plot currently working with is: ")
             console.log(plot)
 
-            let listOfActivities = await getAllActivities() //gets all activity locations from Bumbal
+            let listOfActivities = await getAllActivities() // Get all activity locations from Bumbal
 
-            let activites2 = listOfActivities.map((i) => { // get activities and related zipcode + add a blank zone field with -1 id
+            let activites2 = listOfActivities.map((i) => { // Get activities and related zipcode + add a blank zone field with -1 id
                 return [i.address.latitude, i.address.longitude, i.address.zipcode, -1]; // Lat Lng intensity.
             })
 
-            // go through each activity and find matching zone
+            // Go through each activity and find matching zone
             for (let i = 0; i < activites2.length; i++) {
-                //get the numerical part of a zipcode for each activity
+                // Get the numerical part of a zipcode for each activity
                 let zipcode = parseInt(activites2[i][2].slice(0, 4))
                 for (let j = 0; j < plot.length; j++) {
-                    //for each possible zone, check all zone ranges
+                    // For each possible zone, check all zone ranges
                     for (let k = 0; k < plot[j].length; k++) {
-                        //for each zone range, check whether the zipcode of the activity fits into that zone range
+                        // For each zone range, check whether the zipcode of the activity fits into that zone range
                         if (zipcode >= plot[j][k].zipFrom && zipcode <= plot[j][k].zipTo) {
                             activites2[i][3] = j;
                         }
                     }
                 }
             }
-            //filter out all activities which are depot activities, only driving time activities remain
+            // Filter out all activities which are depot activities, only driving time activities remain
             let activities2Filtered = []
             for (let i = 0; i < activites2.length; i++) {
                 if (activites2[i][3] !== -1) {
                     activities2Filtered.push(activites2[i])
                 }
             }
-            //console.log(activities2Filtered)
-            //array to hold driving time per zone
+            // Array to hold driving time per zone
             let drivingTimeActivities = new Array(plot.length);
-            //array to hold driving distances per zone
+            // Array to hold driving distances per zone
             let drivingDistanceActivities = new Array(plot.length);
-            //array to hold fetch requests HTML per zone
+            // Array to hold fetch requests HTML per zone
             let drivingTimeReqs = new Array(plot.length);
-            //body of the fetch request to be sent out to OSRM
+            // Body of the fetch request to be sent out to OSRM
             const drivingTimeBody = {
                 method: 'GET'
             };
-            //introduce activity counter to see how many activities there are per zone
+            // Introduce activity counter to see how many activities there are per zone
             let countActivities = 0
-            //go through all zones to see what the driving time of activities in that zone is
+            // Go through all zones to see what the driving time of activities in that zone is
             for (let i = 0; i < plot.length; i++) {
-                //go through all activities to find which ones belong to zone i, calculate their driving time
+                // Fo through all activities to find which ones belong to zone i, calculate their driving time
                 countActivities = 0
                 drivingTimeActivities[i] = 0
                 drivingDistanceActivities[i] = 0
                 drivingTimeReqs[i] = "http://router.project-osrm.org/route/v1/driving/"
                 for (let j = 0; j < activities2Filtered.length; j++) {
                     if (activities2Filtered[j][3] === i) {
-                        //update the request for zone i to contain the coordinates of all activities
+                        // Update the request for zone i to contain the coordinates of all activities
                         drivingTimeReqs[i] = drivingTimeReqs[i] + activities2Filtered[j][1] + ',' + activities2Filtered[j][0] + ';'
                         countActivities = countActivities + 1;
 
                     }
                 }
-                //remove last ';' from the request string for driving time in zone i
+                // Remove last ';' from the request string for driving time in zone i
                 drivingTimeReqs[i] = drivingTimeReqs[i].slice(0, -1);
-                //check whether there are less than 2 activities in this zone
+                // Check whether there are less than 2 activities in this zone
                 if (countActivities < 2) {
-                    drivingTimeActivities[i] = 0 //minimal driving time needed in this zone
-                    drivingDistanceActivities[i] = 0 //minimal distance to drive in this zone
+                    drivingTimeActivities[i] = 0 // Minimal driving time needed in this zone
+                    drivingDistanceActivities[i] = 0 //Minimal distance to drive in this zone
                 } else {
-                    //now that the request string for zone i is built, send out fetch request
+                    // Now that the request string for zone i is built, send out fetch request
                     const response = await fetch(drivingTimeReqs[i], drivingTimeBody);
                     const drivingData = await response.json();
-                    //console.log(drivingData)
-                    //using the data from OSRM, compile over all legs of the activities what the total duration is and store it for zone i
+                    // Using the data from OSRM, compile over all legs of the activities what the total duration is and store it for zone i
                     drivingTimeActivities[i] = await getDrivingTime(drivingData)
                     drivingDistanceActivities[i] = await getDrivingDistance(drivingData)
                 }
-
             }
-           // console.log(drivingTimeReqs[1])
-            //console.log(drivingTimeActivities)
-            //console.log(drivingDistanceActivities)
+
             setDrivingTimeActiv(prevDrivingTimeActiv => []);
             setDrivingDistanceActiv(prevDrivingDistanceActiv => []);
 
-            //console.log(drivingTimeReqs[1])
-            //console.log(drivingTimeActivities)
-            //console.log(drivingDistanceActivities)
-
             let totalDrivingTime = 0
             let totalDrivingDistance = 0
+
             for (let i = 0; i < drivingTimeActivities.length; i++) {
                 totalDrivingTime = totalDrivingTime + drivingTimeActivities[i]
                 setDrivingTimeActiv(prevDrivingTimeActiv => [...prevDrivingTimeActiv, drivingTimeActivities[i]]);
                 totalDrivingDistance = totalDrivingDistance + drivingDistanceActivities[i]
                 setDrivingDistanceActiv(prevDrivingDistanceActiv => [...prevDrivingDistanceActiv, drivingDistanceActivities[i]]);
             }
-            setDrivingTime(totalDrivingTime/ 3600)
-            //time to find fuel cost: fuel cost = (litres used * fuel cost)
-            //litres used = driving distance * fuel efficiency
-            setFuelCost(((totalDrivingDistance/ 1000) * averageFuelConsumption) * averageFuelCost)
+            setDrivingTime(totalDrivingTime.toPrecision(3) / 3600)
+            // Time to find fuel cost: fuel cost = (litres used * fuel cost)
+            // Litres used = driving distance * fuel efficiency
+            setFuelCost(((totalDrivingDistance.toPrecision(4) / 1000) * averageFuelConsumption) * averageFuelCost)
             setLoaded(true)
-
         }
 
         initial()
         console.log('updated')
-    }, [zoneId, calculatedZone])
-
+    }, [zoneId, zoneName, calculatedZone])
 
     return (
         <Card title={name} style={{ width: 'fit-content', marginLeft: '0', marginRight: 'auto' }} >
@@ -236,7 +218,7 @@ function TextComponent(props) {
                             </ul>
                         </Panel>
                     </Collapse>
-                    {/* done */}
+                    {/* Done */}
                     <Tooltip title = "This is computed by adding all of the activity times ">
                         <p>Total activity time (hrs): {time}</p>
                     </Tooltip>
@@ -261,12 +243,10 @@ function TextComponent(props) {
                             </ul>
                         </Panel>
                     </Collapse>
-
-
-
                 </div>
             </Spin>
         </Card>
     );
 }
+
 export default TextComponent
