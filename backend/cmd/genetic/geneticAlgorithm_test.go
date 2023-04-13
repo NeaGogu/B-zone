26c3cc1a483c9bc57953397fa352b232775dbd00
Philip Waldman <philip.waldman01@gmail.com>
2023-04-13 14:24:16 +0200

    Added 3 tests in geneticAlgorithm_test.go

--- backend/cmd/genetic/geneticAlgorithm_test.go
+++ backend/cmd/genetic/geneticAlgorithm_test.go
@@ -89,7 +89,7 @@ func makeActivity(t testing.TB, actLat string, actLon string, actZip string, dep
 	return activity
 }
 
-func TestSolution2ZoneModels(t *testing.T) {
+func Test_Solution2ZoneModels(t *testing.T) {
 	tests := []struct {
 		name string
 		sol  Solution
@@ -114,6 +114,187 @@ func TestSolution2ZoneModels(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Solution2ZoneModels(tt.sol)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_checkGenAlgHyperParamsBounds(t *testing.T) {
+	tests := []struct {
+		name    string
+		params  GenAlgHyperParams
+		wantErr bool
+	}{
+		{
+			name:    "all params in bounds",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 42, 0.5, 0.5},
+			wantErr: false,
+		},
+		{
+			name:    "NOffspring = 0",
+			params:  GenAlgHyperParams{0, 42, 42, 42, 42, 0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "NParents = 0",
+			params:  GenAlgHyperParams{42, 0, 42, 42, 42, 0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "NGenerations = 0",
+			params:  GenAlgHyperParams{42, 42, 0, 42, 42, 0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "TournamentSize = 0",
+			params:  GenAlgHyperParams{42, 42, 42, 0, 42, 0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "MaxMutations = 0",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 0, 0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "MutationRate < 0",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 42, -0.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "MutationRate > 1",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 42, 1.5, 0.5},
+			wantErr: true,
+		},
+		{
+			name:    "CrossoverRate < 0",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 42, 0.5, -0.5},
+			wantErr: true,
+		},
+		{
+			name:    "CrossoverRate > 1",
+			params:  GenAlgHyperParams{42, 42, 42, 42, 42, 0.5, 1.5},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := checkGenAlgHyperParamsBounds(tt.params)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func Test_parseActivity(t *testing.T) {
+	tests := []struct {
+		name     string
+		activity models.ActivityModelBumbal
+		want     Pos
+		wantErr  bool
+	}{
+		{
+			name: "valid activity",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{51.452446917398824, 5.462562099535168, 5621},
+			wantErr: false,
+		},
+		{
+			name: "invalid lat",
+			activity: *makeActivity(t, "fhdsalkfjh", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "invalid lon",
+			activity: *makeActivity(t, "51.452446917398824", "dfljsfaljasl", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "zip too short",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "invalid zip",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "kfsadkhl",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseActivity(tt.activity)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_parseDepot(t *testing.T) {
+	tests := []struct {
+		name     string
+		activity models.ActivityModelBumbal
+		want     Pos
+		wantErr  bool
+	}{
+		{
+			name: "valid activity",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "1012 LH"),
+			want:    Pos{52.37660663424486, 4.8972405493159235, 1012},
+			wantErr: false,
+		},
+		{
+			name: "invalid lat",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"dsafhkjl", "4.8972405493159235", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "invalid lon",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "dfshlafjhdk", "1012 LH"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "zip too short",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "1"),
+			want:    Pos{},
+			wantErr: true,
+		},
+		{
+			name: "invalid zip",
+			activity: *makeActivity(t, "51.452446917398824", "5.462562099535168", "5621 HH",
+				"52.37660663424486", "4.8972405493159235", "jkajsdhfh"),
+			want:    Pos{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseDepot(tt.activity)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
 		})
 	}
 }
